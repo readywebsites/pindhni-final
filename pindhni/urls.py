1a4a008392648851cac6bbbfee7c0b79c289d141
fatemadhalech16 <fatemadhalech16@gmail.com>
2026-07-06 13:53:56 +0530

    logo 404

--- pindhni/urls.py
+++ pindhni/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as static_serve
 from api.views import index
 
 # Core backend routes
@@ -14,11 +15,12 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
-# Serve media files in development
+# Serve media and specific static directories in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Add a specific rule to serve the /images directory from dist
+    urlpatterns.append(re_path(r'^images/(?P<path>.*)$', static_serve, {'document_root': settings.BASE_DIR / 'dist/images'}))
 
 # The SPA catch-all route must be the last pattern.
-# It excludes backend-specific paths from being handled by the frontend application.
-# The '?' makes the trailing slash optional in the lookahead.
-urlpatterns.append(re_path(r'^(?!(api/?|admin/?|ckeditor/?|media/?|static/?)).*, index, name='index'))
+# It excludes all backend and static/media paths from being handled by the frontend.
+urlpatterns.append(re_path(r'^(?!(api/?|admin/?|ckeditor/?|media/?|static/?|images/?)).*$', index, name='index'))
