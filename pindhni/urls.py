ddd76d6b8d0332f0ca0e1cee96db135f73cdfe88
fatemadhalech16 <fatemadhalech16@gmail.com>
2026-07-06 13:07:00 +0530

    404 error

--- pindhni/urls.py
+++ pindhni/urls.py
@@ -1,8 +1,5 @@
 """
 URL configuration for pindhni project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
@@ -10,24 +7,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from api.views import index
 
-# Define URL patterns for different parts of the application
-core_urlpatterns = [
+# Core backend routes
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
-static_urlpatterns = []
-# Serve static and media files in development
+# Serve media files in development
 if settings.DEBUG:
-    static_urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    static_urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# The SPA catch-all route should be the last pattern
-# It serves the index.html file for any route that is not an API, admin, or static file route.
-spa_catchall_urlpatterns = [
-    re_path(r'^(?!(api/|admin/|ckeditor/|media/|assets/)).*', index, name='index'),
-]
-
-# Combine all URL patterns
-urlpatterns = core_urlpatterns + static_urlpatterns + spa_catchall_urlpatterns
+# The SPA catch-all route must be the last pattern.
+# It serves the index.html file for any URL that doesn't match one of the backend routes above.
+# Static files should be handled by the WhiteNoise middleware before this point.
+urlpatterns.append(re_path(r'.*', index, name='index'))
